011db7fca9df128b4ff3d3fb1124f267117a4549
Jyo5200 <jyooficial24@gmail.com>
2024-11-15 10:59:01 +0530

    initialize

---
+++ src/main/java/com/firstproject/java_project/App.java
@@ -0,0 +1,10 @@
+package com.firstproject.java_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("22BD1A050D Jyothi Swaroop");
+    }
+}
